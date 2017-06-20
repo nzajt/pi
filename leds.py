8e433dadbfc2ebffbf6bf168374df27b7a676765
Nate <ndaily@netmediagroup.com>
2017-06-19 22:05:17 -0600

    pi: print where it is so I can debug it

--- leds.py
+++ leds.py
@@ -5,12 +5,16 @@ pi = pigpio.pi()
 
 i = 0
 
-while i < 225:
+def light_it_up(i):
     pi.set_PWM_dutycycle(17, i)
     pi.set_PWM_dutycycle(22, i)
     pi.set_PWM_dutycycle(24, i)
+
+while i < 225:
     i += 1
+    light_it_up(i)
     print i
     time.sleep(1)
 
+light_it_up(0)
 pi.stop()
