f7b8ccdf0bebed7ebedf737c501eae4046c6016d
Nate <ndaily@netmediagroup.com>
2017-06-22 08:57:43 -0600

    returning pecentage of day to noon

--- daylight.py
+++ daylight.py
@@ -7,5 +7,8 @@ class Daylight:
 
     @property
     def now(self):
-        return self._now
+        time = self._now
+        percentage_of_day = time.hour / 24.0 + time.minute / (24.0*60.0) + time.second / (24.0*60.0*60.0) + time.microsecond / (24.0*60.0*60.0*1000000.0)
+        return 1.00 - abs(.50 - percentage_of_day)
+
 
