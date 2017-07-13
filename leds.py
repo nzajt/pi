9cdd3853b47b053fe7b3084eb511bf4222e9b65e
Nate <ndaily@netmediagroup.com>
2017-07-13 08:51:13 -0600

    leds: increment by 5 loop forever

--- leds.py
+++ leds.py
@@ -11,12 +11,12 @@ def light_it_up(i):
     pi.set_PWM_dutycycle(24, i)
 
 while True:
-    i += 25
+    i += 5
+    if i > 255:
+        i = 0
     light_it_up(i)
     print i
     time.sleep(1)
-    if i > 255:
-        i = 0
 
 light_it_up(0)
 pi.stop()
