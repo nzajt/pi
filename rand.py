fd72084f7a98dd37a2462fc24362959bef1ae35f
Nate <ndaily@netmediagroup.com>
2017-09-06 08:27:53 -0600

    add class make transition nicer

--- rand.py
+++ rand.py
@@ -2,16 +2,35 @@ import pigpio
 import random
 import time
 
-pi = pigpio.pi()
+class RandomPig:
+    def __init__(self, pin):
+        self.pi = pigpio.pi()
+        self.pin = _pin
+        self.old_value = 0
 
-i = 0
+    def update_value(self, new_value):
+        diff = self.old_value - new_value
+        for i in range(abs(diff)):
+            if diff > 0: # it's positive
+                self.set_pin(self.old_value + i)
+            else: # it's negative
+                self.set_pin(self.old_value - i
+        time.sleep(1)
+        self.old_value = new_value
+        return abs(diff)
 
-def light_it_up(pin):
-    pi.set_PWM_dutycycle(pin, random.randrange(225))
+    def set_pin(self, value):
+        print('Pin %s at value %d' %(self.pin, value))
+        self.pi.set_PWM_dutycycle(self.pin, value)
+
+    def star_loop(self):
+        while True:
+            new_value = random.randrange(225)
+            sleep_time = self.update_value(new_value)
+            time.sleep(sleep_time)
 
 for pin in [17, 22, 24]:
-    while True:
-        light_it_up(pin)
-        time.sleep(random.randrange(10))
+    randp = RandomPig(pin)
+    randp.star_loop()
 
 pi.stop()
