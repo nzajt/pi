dce1a5dd62ed0ff759412fd29449e08e26b508cd
Nate <ndaily@netmediagroup.com>
2017-09-07 09:43:36 -0600

    start and stop scripts

--- kill_leds.sh
+++ kill_leds.sh
@@ -2,9 +2,8 @@
 PIPID=$(ps aux | grep python | awk '{print $2}')
 echo $PIPID 
 kill -9 $PIPID
-ports=( 17, 22, 24 )
-for port in "${array[@]}"
-do
+PORTS=(17 22 24)
+for port in $PORTS; do
   echo $port
-  pigs p ${port} 0
+  pigs p $port 0
 done
