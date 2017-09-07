bbcdf4dcca026f2a2613db401d9f4ea9bdba4510
Nate <ndaily@netmediagroup.com>
2017-09-07 09:39:30 -0600

    start and stop scripts

--- kill_leds.sh
+++ kill_leds.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-kill -9 $(ps aux | grep python | awk '{print $2}')
+PIPID=ps aux | grep python | awk '{print $2}'
+echo $PIPID 
+kill -9 $PIPID
 ports=( 17, 22, 24 )
 for port in "${array[@]}"
 do
